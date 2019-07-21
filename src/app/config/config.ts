f19295a46a164b712985bb5334cc55145560e42c
Ewir Rodriguez <ewirjose1@gmail.com>
2019-07-21 11:40:52 -0400

    Demo de Producción

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
-// export const URL_SERVICIOS = 'https://backend-server-adminpro.herokuapp.com';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://backend-server-adminpro.herokuapp.com';
